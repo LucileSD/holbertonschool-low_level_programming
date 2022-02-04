9d3a857143d6feed44bc2b5377633449eee4ad68
LucileSD <4316@holbertonschool.com>
2022-02-04 14:41:26 +0000

    alphabet lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 { int ch;
-		for (ch = 'A'; ch <= 'Z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			putchar (ch);
 		}
-		putchar ('\n');
 
-		for (ch = 'a'; ch <= 'z'; ch++)
+
+		for (ch = 'A'; ch <= 'Z'; ch++)
 {
 		putchar (ch);
 }

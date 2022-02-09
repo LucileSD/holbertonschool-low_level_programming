55a7cc4f55affad4a9c87216dd292ca926984b58
LucileSD <4316@holbertonschool.com>
2022-02-09 13:04:22 +0000

    integer 2 digits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two integers and returns the result
+ * @a: is 89
+ * @b: is 9
+ *
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	{
+		return (a + b);
+	}
+}

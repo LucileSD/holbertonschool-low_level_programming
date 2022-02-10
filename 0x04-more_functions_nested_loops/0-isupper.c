6bbf7e43597cd78e4eb6311b3b400dad279b3672
LucileSD <4316@holbertonschool.com>
2022-02-10 10:07:44 +0000

    if and return

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isupper - checks for uppercase character
+*@c: define the letter
+*
+*Return : 1 for uppercase and 0 for lowercase
+*/
+int _isupper(int c)
+{
+	if (c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}

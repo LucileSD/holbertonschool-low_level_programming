8e8a6a6cec3912c43fae428d822a002a2cc57529
LucileSD <4316@holbertonschool.com>
2022-02-10 12:11:17 +0000

    except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_most_numbers - prints the numbers, from 0 to 9,
+*followed by a new line
+*
+*Return: None
+*/
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 50 && n != 52)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}

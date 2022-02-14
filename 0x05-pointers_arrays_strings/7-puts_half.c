f4b565a16bfd1ab93158e07a649dfba1d3d5ae5d
LucileSD <4316@holbertonschool.com>
2022-02-14 16:23:43 +0000

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*puts_half - print the second half of the string
+*@str: represent the string
+*
+*Return: None
+*/
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	len = len / 2;
+
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+
+	}
+	_putchar('\n');
+}

d19362661097f29109005d6a7f8019722664cf18
LucileSD <4316@holbertonschool.com>
2022-02-14 15:54:39 +0000

    even numbers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *puts2 - prints every other character of a string
+ *
+ *@str: represent character
+ *
+ *Return: None
+ */
+void puts2(char *str)
+{
+	int p = 0;
+
+	while (str[p] != '\0')
+	{
+		if (p % 2 == 0)
+		{
+			_putchar(str[p]);
+		}
+		p++;
+	}
+	_putchar('\n');
+}

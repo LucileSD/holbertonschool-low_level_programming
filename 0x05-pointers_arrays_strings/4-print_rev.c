e4417db570e53461ab2b33b9444bbed0a0b933a9
LucileSD <4316@holbertonschool.com>
2022-02-14 13:35:07 +0000

    lenght aqnd reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_rev - prints a string, in reverse
+*@s: represent the string
+*
+*Return: None
+*/
+void print_rev(char *s)
+{
+	int p = 0;
+
+	while (s[p] != '\0')
+	{
+		p++;
+	}
+
+
+	while (p >= 0)
+	{
+		_putchar(s[p]);
+		p--;
+	}
+	_putchar('\n');
+}

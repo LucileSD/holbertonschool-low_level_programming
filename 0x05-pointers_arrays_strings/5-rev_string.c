e97600f4672d92c53e9ff7450233a38c12bd22c6
LucileSD <4316@holbertonschool.com>
2022-02-14 15:27:02 +0000

    forward and backward

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*rev_string - function that reverses a string
+*@s: reprsent the string
+*
+*Return: None
+*/
+void rev_string(char *s)
+{
+	int lent = 0, forw = 0, rev = 0;
+	char tmp;
+
+	while (s[lent] != 0)
+	{
+		lent++;
+	}
+
+	rev = lent - 1;
+
+	while (rev > lent / 2)
+	{
+		tmp = s[rev];
+		s[rev] = s[forw];
+		s[forw] = tmp;
+		rev--;
+		forw++;
+	}
+}

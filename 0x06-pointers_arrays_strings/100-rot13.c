6f48a631641f1d93e65bb54f488e5e65d9416643
LucileSD <4316@holbertonschool.com>
2022-02-17 15:30:03 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *rot13 - encodes a string using rot13
+ *@tab: the array
+ *Return: tab
+ */
+char *rot13(char *tab)
+{
+	int n, l;
+	char low[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char upp[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (n = 0; tab[n] != '\0'; n++)
+	{
+		for (l = 0; low[l] != '\0'; l++)
+		{
+			if (tab[n] == upp[l])
+			{
+				;
+			}
+		}
+	}
+	return (tab);
+}

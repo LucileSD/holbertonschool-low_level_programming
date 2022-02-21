1e67f0e1b98a608809546da3b8dc861e3211761d
LucileSD <4316@holbertonschool.com>
2022-02-21 13:06:47 +0000

    compare

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,14 +7,14 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j; 
+	int i, j;
 	unsigned int count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 
 		for (j = 0; accept[j] != '\0'; j++)
-		{			
+		{
 			if (s[i + j] == accept[j])
 			{
 				count++;

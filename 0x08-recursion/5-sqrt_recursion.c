cfaefb13944cbacecbf54f463515846a4d0a47df
LucileSD <4316@holbertonschool.com>
2022-02-24 13:23:01 +0000

    sqrt root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,9 +2,27 @@
 /**
  *_sqrt_recursion - returns the natural square root of a number
  *@n: the number
- *@Return: n
+ *Return: n
  */
 int _sqrt_recursion(int n)
 {
-	if (n != 0)
-		return (-1)
+	if (n < 0)
+		return (-1);
+	else
+		return (_sqrt(n, 1));
+}
+
+/**
+ *_sqrt - returns the result
+ *@n: the number
+ *@y: test the answer
+ *Return: y
+ */
+int _sqrt(int n, int y)
+{
+	if (y * y == n)
+		return (y);
+	if (y * y > n)
+		return (-1);
+	return (_sqrt(n, y + 1));
+}

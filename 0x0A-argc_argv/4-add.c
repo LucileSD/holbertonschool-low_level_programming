353532f5ec9f31ef715f63e7248a78bacc52d32d
LucileSD <4316@holbertonschool.com>
2022-02-25 16:37:17 +0000

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,27 +2,34 @@
 #include <stdlib.h>
 /**
  *main - adds positive numbers
- *@argc - count argument
- *@argv - the array of arguments
+ *@argc: count argument
+ *@argv: the array of arguments
  *Return: the result or error
  */
 int main(int argc, char *argv[])
 {
-	int num, result = 0;
+	int num1, num2, result = 0;
 
-
-	for (num = 1; num < argc; num++)
+	if (argc > 1)
 	{
-		result += atoi(argv[num]);
+		for (num1 = 1; num1 < argc; num1++)
+		{
+			result += atoi(argv[num1]);
+
+			for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+			{
+				if (argv[num1][num2] < 48 || argv[num1][num2] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
 		printf("%d\n", result);
 	}
-	if (argc < 3)
+	else
 	{
 		printf("0\n");
-	}
-	else if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122))
-	{
-		printf("Error\n");
 	}
 	return (0);
 }

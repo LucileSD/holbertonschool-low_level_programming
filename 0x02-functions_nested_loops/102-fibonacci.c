fdc6cd0109b956556680f6469f0f219bee2266a3
LucileSD <4316@holbertonschool.com>
2022-02-28 15:31:19 +0000

    fib

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,18 +7,18 @@
 int main(void)
 {
 	int i;
-	long int c = 1, a = 0, b = 1;
+	long int c, a = 0, b = 1;
 
 	for (i = 3; i <= 50; i++)
 	{
 		c = a + b;
 		a = b;
 		b = c;
+		printf("%ld", c);
 		if (i != 50)
 		{
 			printf("%ld, ", c);
 		}
-		printf("%ld", c);
 	}
 	printf("\n");
 	return (0);

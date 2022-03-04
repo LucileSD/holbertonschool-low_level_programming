0c386a596f483a0f0418d9975b34a6d5ee9faef6
LucileSD <4316@holbertonschool.com>
2022-03-04 22:02:17 +0000

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ *_realloc - reallocates a memory block using malloc and free
+ *@ptr: the pointer to the memory previously
+ *allocated
+ *@old_size: the size il bytes of the allocated
+ *space for ptr
+ *@new_size: the size of the new memory block
+ *Return: None
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr = NULL;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free (ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		free (new_ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		_memcpy(new_ptr, ptr, old_size);
+	}
+	if (ptr == NULL)
+	{
+		_memcpy(new_ptr, ptr, new_size);
+	}
+	free (ptr);
+	return (new_ptr);
+}
+#include "main.h"
+/**
+ *_memcpy - copies memory area
+ *@dest: destination
+ *@src: source
+ *@n: number of bytes
+ *Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}

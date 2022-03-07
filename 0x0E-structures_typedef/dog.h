17e98de11fb0332c04b1f0d6ec72882c1df7012f
LucileSD <4316@holbertonschool.com>
2022-03-07 12:37:49 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - know information about the dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+#endif

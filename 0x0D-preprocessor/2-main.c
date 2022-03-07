d2186e167961d03e193b47db996e5a0e696a8b59
LucileSD <4316@holbertonschool.com>
2022-03-07 10:20:10 +0000

    print name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define NAMEOFFILE __FILE__
+
+/**
+ *main - print name of file
+ *Return: None
+ */
+int main(void)
+{
+	printf("%s\n", NAMEOFFILE);
+	return (0);
+}

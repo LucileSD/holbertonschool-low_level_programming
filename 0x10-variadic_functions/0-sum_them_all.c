4f861dd4cc86343331de5f51785f1039916817f0
LucileSD <4316@holbertonschool.com>
2022-03-10 09:51:49 +0000

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 /**
  * sum_them_all - returns the sum of all its parameters
- * @n: the arguments
+ * @n: the number of arguments
  * Return: the sum
  */
 int sum_them_all(const unsigned int n, ...)

856f85e206ffc5e042bf738771ffc262defc1347
LucileSD <4316@holbertonschool.com>
2022-03-20 14:38:04 +0000

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * premain - print before main fiunction
+ */
+void premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

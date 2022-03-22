b884a38533298d38ae03f1fa66d885c18749b9df
LucileSD <4316@holbertonschool.com>
2022-03-22 13:59:37 +0000

    add node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,5 +29,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = temp->next;
 		temp->next = new;
 	}
+	else
+	{
+		new->next = *head;
+		*head = new;
+	}
 	return (new);
 }

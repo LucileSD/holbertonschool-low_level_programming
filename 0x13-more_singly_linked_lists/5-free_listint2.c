69e1a48d9a1c47d069ba6e65b9f0f1def3c10352
LucileSD <4316@holbertonschool.com>
2022-03-22 08:49:45 +0000

    non pointer

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,14 +12,11 @@ void free_listint2(listint_t **head)
 		return;
 	}
 
-	else
+	while (temp != NULL)
 	{
-		while (temp != NULL)
-		{
-			temp = (*head)->next;
-			free(*head);
-			*head = temp;
-		}
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 	head = NULL;
 }

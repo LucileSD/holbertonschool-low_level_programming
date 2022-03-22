5bd7757130f48d0cbbfe1a8c5ebbea9ccf70a1c9
LucileSD <4316@holbertonschool.com>
2022-03-22 09:20:19 +0000

    null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,7 +5,7 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
 	if (head == NULL || *head == NULL)
 	{
@@ -14,6 +14,7 @@ void free_listint2(listint_t **head)
 
 	while (*head != NULL)
 	{
+		temp = *head;
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;

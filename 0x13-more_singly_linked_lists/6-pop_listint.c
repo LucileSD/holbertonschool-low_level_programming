3412f651f8ccbd17ae3451d47e54f8df102cc901
LucileSD <4316@holbertonschool.com>
2022-03-22 10:11:08 +0000

    free head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,4 +7,13 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int data = (*head)->n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	return (data);
 }

8740609ed5623183de88cfbb3a486a71c676af4c
LucileSD <4316@holbertonschool.com>
2022-03-24 11:27:14 +0000

    len

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,12 +11,12 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int len;
 
-	len = strlen(b);
+	len = strlen(b) - 1;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = len - 1; i >= 0; i--)
+	for (i = len; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);

9c663e153e8e5299347732dfa253e54c5cc6365e
LucileSD <4316@holbertonschool.com>
2022-03-24 11:30:15 +0000

    strlen

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,4 +5,7 @@
 #include <string.h>
 #include <math.h>
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int _strlen(const char *s);
 #endif

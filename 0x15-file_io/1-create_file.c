da9234c6356b91290a6b51a5ca51b715ec573a47
LucileSD <4316@holbertonschool.com>
2022-03-28 11:07:48 +0000

    trunc

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	creat_file = open(filename, O_RDWR | O_CREAT | O_CREAT, 0600);
+	creat_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (creat_file < 0)
 		return (-1);
 

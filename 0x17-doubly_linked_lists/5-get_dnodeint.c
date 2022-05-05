4991169f867d308bcca74551f353aa69cdd14a2c
LucileSD <4316@holbertonschool.com>
2022-05-05 11:16:42 +0000

    head exist

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int count = 0;
 	dlistint_t *tmp = head;
 
-	if (head)
+	if (!head)
+		return (NULL);
+	else
 	{
 		while (count < index)
 		{

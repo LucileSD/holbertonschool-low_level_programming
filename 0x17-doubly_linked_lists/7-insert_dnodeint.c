f511ca7dcc0075a681b9b7c9c0eb29c237a1e199
LucileSD <4316@holbertonschool.com>
2022-05-05 13:11:28 +0000

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 /**
+ * dlistint_len - count the number of elements in a linked dlistint_t list
+ * @h: the head of the list
+ * Return: the number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *tmp = h;
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (tmp)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+	return (count);
+
+}
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: the head of the list
  * @idx: the index of the list where the new node should be added
@@ -9,7 +29,17 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *before, *tmp = *h;
+	unsigned int len = dlistint_len(*h);
 	unsigned int count = 0;
+
+	if (idx > len)
+		return (NULL);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	if (idx == len)
+		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
@@ -25,8 +55,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			tmp = tmp->next;
 			count++;
-			if (tmp == NULL)
-				return (NULL);
 		}
 
 		before = tmp->prev;

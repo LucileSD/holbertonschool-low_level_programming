cbd7c0b21474cc6dd9abccc2af8de54dcc48eab2
LucileSD <4316@holbertonschool.com>
2022-05-05 12:47:31 +0000

    indent

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,11 +29,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				return (NULL);
 		}
 
-	before = tmp->prev;
-	tmp->prev = new_node;
-	new_node->next = tmp;
-	before->next = new_node;
-	new_node->prev = before;
+		before = tmp->prev;
+		tmp->prev = new_node;
+		new_node->next = tmp;
+		before->next = new_node;
+		new_node->prev = before;
 	}
 	return (new_node);
 }

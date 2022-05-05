a1d90f0ba92881b1bacf90f5c5397d14a83963ad
LucileSD <4316@holbertonschool.com>
2022-05-05 08:03:25 +0000

    betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
+ * dlistint_len - count the number of elements in a linked dlistint_t list
  * @h: the head of the list
- * Return: the number of node
+ * Return: the number of elements
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *tmp = h;
 	int count = 0;
@@ -13,9 +13,9 @@ size_t print_dlistint(const dlistint_t *h)
 		return (0);
 	while (tmp)
 	{
-		printf("%d\n", tmp->n);
 		tmp = tmp->next;
 		count++;
 	}
 	return (count);
+
 }

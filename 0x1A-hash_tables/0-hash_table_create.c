ffa9d91927e1424d298592580f0d48188bfdcf21
LucileSD <4316@holbertonschool.com>
2022-05-25 11:52:03 +0000

    malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,17 +1,24 @@
 #include "hash_tables.h"
 /**
  * hash_table_create - creates a hash table
- * @size: the size of the array
+ * @size: the size of the hash table
  * Return: a pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *array;
+	hash_table_t *ht;
 
-	array = malloc(sizeof(hash_table_t) * size);
-
-	if (!array)
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
 		return (NULL);
 
-	return (array);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t) * size);
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	return (ht);
 }

a0deb01a022f5706fddcddbd7effc78f746570eb
LucileSD <4316@holbertonschool.com>
2022-05-27 08:47:10 +0000

    comma

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *tmp = NULL;
+	char *empty = "", *comma = ", ";
+
+	if (!ht)
+		return;
+	
+	printf("{");
+	while (index < ht->size)
+	{
+		tmp = ht->array[index];
+		while (tmp)
+		{
+			printf("%s", empty);
+			printf("'%s' : '%s'",tmp->key, tmp->value);
+			empty = comma;
+			tmp = tmp->next;
+		}
+		index += 1;
+	}
+	printf("}\n");
+}

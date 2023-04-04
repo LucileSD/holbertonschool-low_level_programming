97d1be1ee8d61ad740bcacd906684125bea8c26d
LucileSD <4316@holbertonschool.com>
2023-04-04 17:41:06 +0200

    do it again

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,5 +1,4 @@
 #include "search.h"
-
 /**
 * linear_skip - Linear search in a skip list
 * @list: a pointer to the head of the skip list to search in
@@ -17,28 +16,29 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	{
 		prev = current;
 		current = current->express;
-		printf("Value checked at index [%ld]= [%d]\n",
+		printf("Value checked at index [%ld] = [%d]\n",
 				current->index, current->n);
 	}
-
-	if (value < current->n)
+	if (current->n < value)
 	{
-		printf("Value found between indexes [%ld] and [%ld]\n",
-				prev->index, current->index);
-	}
-	else
-	{
-		while (current->next && current->n < value)
+		prev = current;
+		while (current->next)
 			current = current->next;
-		printf("Value found between indexes [%ld] and [%ld]\n",
-				prev->index, current->index);
 	}
-	while (prev->n < value && prev->next)
+	printf("Value found between indexes [%ld] and [%ld]\n",
+				prev->index, current->index);
+
+	while (prev->next && prev->n < value)
 	{
-		printf("Value checked at index [%ld]= [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+		printf("Value checked at index [%ld] = [%d]\n",
+				prev->index, prev->n);
 		prev = prev->next;
 	}
-	printf("Value checked at index [%ld]= [%d]\n", prev->index, prev->n);
+	printf("Value checked at index [%ld] = [%d]\n",
+				prev->index, prev->n);
+
 	if (prev->n == value)
 		return (prev);
 
